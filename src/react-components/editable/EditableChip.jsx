31685141179bd943a8c76b1c96d90af9d44741de
andrei.alexandru <andrei.alexandru@cgm.com>
2024-02-09 11:59:45 +0200

    Editable chip textfield

--- src/react-components/editable/EditableChip.jsx
+++ src/react-components/editable/EditableChip.jsx
@@ -2,18 +2,65 @@ import * as React from 'react';
 import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
 import {useState} from "react";
+import {TextField} from "@mui/material";
 
 const EditableChip = () => {
 
-    const [deleteVisible, setDeleteVisible] = useState(true);
+    const [chipVisible, setChipVisible] = useState(true);
+    const [textFieldVisible, setTextFieldVisible] = useState(false);
+    const [text, setText] = useState('Romania');
+    const [label, setLabel] = useState('Romania');
+
+    const handleClick = () => {
+        setTextFieldVisible(true);
+        setLabel('');
+    }
+
+    const handleTextFieldBlur = () => {
+        setLabel(text);
+        setTextFieldVisible(false);
+    }
 
     const handleDelete = () => {
-        setDeleteVisible(false);
+        setChipVisible(false);
+        setTextFieldVisible(false);
     };
+
+    const handleTextChange = (event) => {
+        setText(event.target.value);
+    }
 
     return (
         <Stack direction="row" spacing={1}>
-            {deleteVisible && <Chip role="button" title="delete" label="Deletable" onDelete={handleDelete} />}
+            <div className="editable-chip" style={{ position: 'relative' }}>
+                {chipVisible && <Chip role="button"
+                                      style={{ minWidth: '160px' }}
+                                      sx={{
+                                          '& .MuiChip-label': {
+                                            minWidth: '100px'
+                                          },
+                                          '& .MuiChip-deleteIcon': {
+                                          },
+                                      }}
+                                      title="Romania"
+                                      label={label}
+                                      onClick={handleClick}
+                                      onDelete={handleDelete} />
+                }
+                {textFieldVisible && <TextField id="chip-text"
+                                                style={{ position: 'absolute', left: '16px', width: '128px', }}
+                                                sx={{
+                                                    '& .MuiInputBase-input': {
+                                                        fontSize: '14px',
+                                                        marginTop: '2px'
+                                                    }
+                                                }}
+                                                variant="standard"
+                                                value={text}
+                                                onChange={handleTextChange}
+                                                onBlur={handleTextFieldBlur}/>
+                }
+            </div>
         </Stack>
     );
 }
